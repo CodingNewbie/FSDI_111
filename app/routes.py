e5dd567136b6383f6ce8a3cf426fc700e4d7b07f
Adam <adamrayschmidt@gmail.com>
2024-04-17 12:58:59 -0700

    Initial commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from flask import Flask     # from the flask module import the Flask class
+
+app = Flask(__name__)       # Create an instance of the Flask class
+
+@app.get("/aboutme")        # Flask decorator that maps view functions to routes
+def aboutMe():              # View function
+    me = {                  # Python dictionary
+        "first_name": "Adam",
+        "last_name": "Schmidt",
+        "hobbies": "Coding",
+        "is_online": True
+    }
+    return me               # When you return a dict from a view function, it becomes JSON
+

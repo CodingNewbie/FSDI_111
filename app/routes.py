35666cf84b095edcf54d431f25a8bdd8db211a6d
Adam <adamrayschmidt@gmail.com>
2024-04-17 19:46:28 -0700

    Add sandbox.db and create user and vehicle tables.

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask     # from the flask module import the Flask class
 
 app = Flask(__name__)       # Create an instance of the Flask class
 
+@app.get("/")
 @app.get("/aboutme")        # Flask decorator that maps view functions to routes
 def aboutMe():              # View function
     me = {                  # Python dictionary
